4e4d5042b4ce81781fb81103c1d1cbe1fb06b72c
Ruth Leene <ruthleene@MacBook-Air-6.local>
2026-03-05 15:36:14 +0530

    Initial commit

---
+++ sql_load/1_create_database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE sql_course;
+git status
+
+-- DROP DATABASE iIF EXISTS sql_course;
+
+git config --global user.name "Ruth Leene"
+git config --global user.email "ruth.2103.leene@email.com"
+git add .
+git commit -m "Initial commit"
+git config --global user.name "ruthleene"
